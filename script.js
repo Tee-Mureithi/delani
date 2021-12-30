a8ae78df010fbe71cfa4348de183e5bb45f5dd3b
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2021-12-30 16:12:40 +0300

    it will make the website responsive

---
+++ script.js
@@ -0,0 +1,61 @@
+const designIcon = document.querySelector('.iconDesign')
+const developmentIcon = document.querySelector('.devIcon')
+const projectIcon = document.querySelector('projIcon')
+           
+          $(document).ready(function(){
+                 $(".design").click(function() {
+                 $(".description").toggle();
+                 designIcon.style.display="none"
+
+            });
+           });
+
+           $(document).ready(function(){
+                 $(".development").click(function() {
+                 $(".dev").toggle();
+                 developmentIcon.style.display='none'
+
+            });
+            
+           });
+
+           $(document).ready(function(){
+                 $(".management").click(function() {
+                 $(".project").toggle();
+                 projIcon.style.display='none'
+
+            },
+            function(){
+
+                projIcon.style.display='center'
+            })
+           });
+
+
+        //   $(document) .ready(function() {
+        //     $(".management").click(function(image){
+        //         $(".project").toggle();
+        //         image.currentTarget.style.display='none'
+        //     },
+        //     function(image){
+        //         image.currentTarget.style.display='block'
+        //     }
+        //     )
+        //   })
+
+        //   $(".project").hover(
+        //     (e) => {
+        //       const project = e.currentTarget;
+        //       const projectImg = project.querySelector(".project__img");
+        //       const projectCaption = project.querySelector(".project__title");
+        
+        //       projectImg.classList.add("dull");
+        //       projectCaption.classList.add("displayCaption");
+        //       document.querySelector('.image-title').style.border= "1px solid red"
+
+        //     },
+
+        //     (e) => {
+        //       const project = e.currentTarget;
+        //       const projectImg = project.querySelector(".project__img");
+        //       const projectCaption = project.querySelector(".project__title");
