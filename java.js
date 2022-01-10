e3017ed995f6319dd577160bc206220c6c1cd194
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2022-01-10 10:53:57 +0300

    will modify the toggle function

--- java.js
+++ java.js
@@ -1,39 +1,38 @@
 
 
-
-$(document).ready(function(){
-    $(".design").click( function(){
- 
-    $(".description").toggle();
-       
-    },
-
-    )
-})
-
-$(document).ready(function(){
-    $(".development").click( function(){
+$(document).ready(function () {
+    $(".iconDesign").click(function () {
+      $(this).toggle(function () {
+        $(".description").toggle();
+      });
+    });
+    $(".description").click(function () {
+      $(this).toggle(function () {
+        $(".iconDesign").toggle();
+      });
+    });
+  });
+  $(document).ready(function () {
+    $(".devIcon").click(function () {
+      $(this).toggle(function () {
         $(".dev").toggle();
-        
-    },
-
-    )
-})
-
-$(document).ready(function(){
-    $(".management").click( function(){
+      });
+    });
+    $(".dev").click(function () {
+      $(this).toggle(function () {
+        $(".devIcon").toggle();
+      });
+    });
+  });
+  $(document).ready(function () {
+    $(".product_icon").click(function () {
+      $(this).toggle(function () {
         $(".project").toggle();
-        
-    },
-
-    )
-})
-
-
-
-function validateForm(){
-    var name
-    document.getElementById("#name").innerHTML= name
-    
-}
-alert( + "your message has been received")
+      });
+    });
+    $(".project").click(function () {
+      $(this).toggle(function () {
+        $(".product_icon").toggle();
+      });
+    });
+  });
